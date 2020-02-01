3200ce4932743ff9a2cf67e6c2dc5d7213fb63ae
flask-pro <fadeev@stantis.ru>
2020-02-01 15:16:40 +0300

    Add cli command `about`. (#2)
    closes #1

--- simple/simple_flask/main.py
+++ simple/simple_flask/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import click
 
 app = Flask(__name__)
 
@@ -9,6 +10,12 @@ def index():
     """
     template_name = 'simple_flask'
     return render_template('index.html.j2', template_name=template_name)
+
+
+@app.cli.command('about')
+def hello_command():
+    click.echo(f'This is template "flask_simple"!')
+
 
 if __name__ == "__main__":
     app.run()
